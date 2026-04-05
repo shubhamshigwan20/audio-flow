c9db34b9830b5bd2b9d5e52be78d1faf3524da24
Shubham Shigwan <shubhamonline024@gmail.com>
2026-04-06 00:13:56 +0530

    added error for file size too large

--- backend/middleware/error.middleware.js
+++ backend/middleware/error.middleware.js
@@ -1,8 +1,18 @@
+const multer = require("multer");
 const errorHandler = async (err, req, res, next) => {
   if (!err) {
     next();
   }
   console.log("error ->", err);
+
+  if (err instanceof multer.MulterError) {
+    if (err.code === "LIMIT_FILE_SIZE") {
+      return res.status(413).json({
+        status: false,
+        message: "file too large. maximum allowed size is 1 GB.",
+      });
+    }
+  }
   return res.status(500).json({
     status: false,
     message: "internal server error",
