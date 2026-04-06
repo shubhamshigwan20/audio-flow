3d3477ad41a38c3f223b94e9b55205574e2c85ba
Shubham Shigwan <shubhamonline024@gmail.com>
2026-04-07 02:53:24 +0530

    added header

---
+++ frontend/src/components/common/header/Header.tsx
@@ -0,0 +1,34 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu"
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between border-b px-5 py-2">
+      <div className="text-2xl font-semibold">Audio Flow</div>
+      <div className="font-semibold">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink>Transcribe</NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink>My Jobs</NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink>Observe</NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink>DLQ</NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </div>
+  )
+}
+
+export default Header
