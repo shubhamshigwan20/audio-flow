806a955c5d61400aaf41c080560aa8cfe2d522a2
Shubham Shigwan <shubhamonline024@gmail.com>
2026-04-11 02:35:40 +0530

    added support for google drive

--- frontend/src/constants/endpoints.tsx
+++ frontend/src/constants/endpoints.tsx
@@ -6,4 +6,15 @@ const GET_TRANSCRIPT = (id: string) => `/jobs/${id}/transcript` //GET
 
 const TRANSCRIBE = `/transcribe` //POST
 
-export { JOB_HISTORY, JOB_STATUS, GET_TRANSCRIPT, TRANSCRIBE }
+const AUTH_GOOGLE_TOKEN = `/auth/google-token` //POST
+
+const TRANSCRIBE_GOOGLE_DRIVE = `/transcribe-gdrive` //POST
+
+export {
+  JOB_HISTORY,
+  JOB_STATUS,
+  GET_TRANSCRIPT,
+  TRANSCRIBE,
+  AUTH_GOOGLE_TOKEN,
+  TRANSCRIBE_GOOGLE_DRIVE,
+}
--- frontend/src/views/home/Home.tsx
+++ frontend/src/views/home/Home.tsx
@@ -6,10 +6,29 @@ import { Button } from "@/components/ui/button"
 import { allowedMimeType } from "@/constants/constants"
 import AlertBox from "./components/AlertBox"
 import useLoaderState from "@/store/LoaderStateStore"
-import { TRANSCRIBE } from "@/constants/endpoints"
+import {
+  AUTH_GOOGLE_TOKEN,
+  TRANSCRIBE,
+  TRANSCRIBE_GOOGLE_DRIVE,
+} from "@/constants/endpoints"
 import { Wand2 } from "lucide-react"
 
 const MAX_FILE_SIZE = 500
+
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
+const API_KEY = import.meta.env.VITE_API_KEY
+console.log("ad122", API_KEY)
+
+type Docs = { id: string; name: string; mimeType: string }
+
+type codeResponseType = {
+  code: string
+}
+
+type pickerCallbackDataType = {
+  action: string
+  docs?: Docs[]
+}
 
 const Home = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null)
@@ -34,6 +53,116 @@ const Home = () => {
       setUploading(false)
       setIsOpen(false)
       console.log(err)
+    }
+  }
+
+  async function pickerCallback(
+    data: pickerCallbackDataType,
+    refreshToken: string
+  ) {
+    if (data.action === google.picker.Action.PICKED) {
+      const file = data.docs?.[0]
+      console.log("file -->", file)
+      if (!file?.id) {
+        return
+      }
+
+      // Validate it's an audio file on frontend too
+      const allowedMimeTypes = [
+        "audio/mpeg",
+        "audio/mp3",
+        "audio/wav",
+        "audio/x-wav",
+        "audio/ogg",
+        "audio/mp4",
+      ]
+
+      if (!allowedMimeTypes.includes(file.mimeType)) {
+        alert("Please select an audio file only")
+        return
+      }
+
+      const payload = {
+        filename: data.docs?.[0]?.name,
+        fileid: file?.id,
+        refreshToken: refreshToken,
+        mimeType: file?.mimeType,
+      }
+
+      setIsOpen(true)
+      try {
+        const result = await api.post(TRANSCRIBE_GOOGLE_DRIVE, payload)
+        setIsOpen(false)
+        setJobId(result.data.jobId)
+      } catch (err) {
+        console.log(err)
+        setJobId("error")
+      } finally {
+        setIsOpen(false)
+      }
+    }
+  }
+
+  function openPicker(accessToken: string, refreshToken: string) {
+    if (!API_KEY) {
+      return
+    }
+    gapi.load("picker", () => {
+      const picker = new google.picker.PickerBuilder()
+        .addView(
+          new google.picker.View(google.picker.ViewId.DOCS).setMimeTypes(
+            [
+              "audio/mpeg", // .mp3
+              "audio/mp3", // fallback
+              "audio/wav", // .wav
+              "audio/x-wav", // fallback
+              "audio/ogg", // .ogg
+              "audio/mp4", // .m4a
+              "audio/aac", // .aac
+              "audio/flac", // .flac
+            ].join(",")
+          )
+        )
+        .setOAuthToken(accessToken)
+        .setDeveloperKey(API_KEY)
+        .setCallback((data: pickerCallbackDataType) => {
+          pickerCallback(data, refreshToken)
+        })
+        .build()
+
+      picker.setVisible(true)
+    })
+  }
+
+  const handleGoogleDriveUpload = async () => {
+    if (!CLIENT_ID) {
+      return
+    }
+
+    const existingAccessToken = localStorage.getItem("googleAccessToken")
+    const existingRefreshToken = localStorage.getItem("googleRefreshToken")
+
+    if (existingRefreshToken && existingAccessToken) {
+      openPicker(existingAccessToken, existingRefreshToken)
+      return
+    } else {
+      const client = google.accounts.oauth2.initCodeClient({
+        client_id: CLIENT_ID,
+        scope: "https://www.googleapis.com/auth/drive.readonly",
+        callback: async (codeResponse: codeResponseType) => {
+          const payload = {
+            code: codeResponse.code,
+          }
+          const result = await api.post(AUTH_GOOGLE_TOKEN, payload)
+          if (result.status === 200) {
+            localStorage.setItem("googleAccessToken", result.data.accessToken)
+            localStorage.setItem("googleRefreshToken", result.data.refreshToken)
+            openPicker(result.data.accessToken, result.data.refreshToken)
+          }
+        },
+      })
+
+      client.requestCode()
     }
   }
 
@@ -69,6 +198,16 @@ const Home = () => {
             validFile={validFile}
           />
         )}
+        <div className="flex justify-center">
+          <Button
+            className="w-full gap-2 p-2"
+            onClick={handleGoogleDriveUpload}
+          >
+            <div className="flex h-4 items-center gap-3 p-0">
+              <i className="fab fa-google-drive fa-2x"></i> Google Drive Upload
+            </div>
+          </Button>
+        </div>
 
         <Button
           className="w-full gap-2"
